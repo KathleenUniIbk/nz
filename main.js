92ffd9d0fe01ff18a3d2c2abc9cd1e3a9cdef8a3
Kathleen Delp <kathleen.delp@student.uibk.ac.at>
2025-03-27 16:32:40 +0100

    stop.

--- main.js
+++ main.js
@@ -2,12 +2,17 @@
 Skript für Neuseelandreise
 */
 
-let lat = -45.031389;
-let lng = 168.660833;
-let zoom = 14;
+let stop = {
+  nr: 17,
+  title: "Queenstown",
+  user: "Kathleenuniibk",
+  lat: -45.031389,
+  lng: 168.660833,
+  zoom:14,
+};
 
 //Karte initalisieren
-let map = L.map('map').setView([lat, lng], zoom);
+let map = L.map('map').setView([stop.lat, stop.lng], stop.zoom);
 
 // Hintergrundkarte definieren
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -16,13 +21,14 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 //Marker zeichnen
-let marker = L.marker([lat, lng]).addTo(map);
+let marker = L.marker([stop.lat, stop.lng]).addTo(map);
 
 //Popup definieren und öffnen
 marker.bindPopup(`
-      <h2>Queenstown</h2>
+      <h2>${stop.title}</h2>
       <ul> 
-      <li>Geogr. Breite: ${lat.toFixed(5)}° </li>
-      <li>Geogr. Länge: ${lng.toFixed(5)}° </li>
+      <li>Geogr. Breite: ${stop.lat.toFixed(5)}° </li>
+      <li>Geogr. Länge: ${stop.lng.toFixed(5)}° </li>
       </ul>
       `).openPopup();
+
