ea162317fe7e1d0c5ddcaf606a256673d9ddfac1
Kathleen Delp <kathleen.delp@student.uibk.ac.at>
2025-03-27 15:49:53 +0100

    drei variablen für lat, lng und zoom verwendet

--- main.js
+++ main.js
@@ -2,9 +2,12 @@
 Skript für Neuseelandreise
 */
 
-//Karte initalisieren
+let lat =  -45.031389;
+let lng = 168.660833;
+let zoom = 14;
 
-let map = L.map('map').setView([-45.031389, 168.660833], 14);
+//Karte initalisieren
+let map = L.map('map').setView([lat, lng], zoom);
 
 // Hintergrundkarte definieren
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -13,7 +16,7 @@ let map = L.map('map').setView([-45.031389, 168.660833], 14);
     }).addTo(map);
 
     //Marker zeichnen
-    let marker = L.marker([-45.031389, 168.660833]).addTo(map);
+    let marker = L.marker([lat, lng]).addTo(map);
 
     //Popup definieren und öffnen
     marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
