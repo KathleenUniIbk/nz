e4cf439baa59fd2583e9b7d9a457e7a10828a97f
Kathleen Delp <kathleen.delp@student.uibk.ac.at>
2025-03-27 15:36:16 +0100

    Kommentare ergänzt

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
+/*
+Skript für Neuseelandreise
+*/
+
+//Karte initalisieren
+
 var map = L.map('map').setView([-45.031389, 168.660833], 14);
+
+// Hintergrundkarte definieren
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+
+    //Marker zeichnen
     var marker = L.marker([-45.031389, 168.660833]).addTo(map);
+
+    //Popup definieren und öffnen
     marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
